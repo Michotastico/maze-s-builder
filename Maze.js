2845ef406d3476b4d250535f40ae5fb59038ff0a
Micho Llorens <mllorens@dcc.uchile.cl>
2015-07-29 15:14:10 -0300

    Added other methods to Maze

--- Maze.js
+++ Maze.js
@@ -7,16 +7,18 @@ class Maze{
 	}
 
 	generate(){
-		this.maze.changeinrange((0,0),(0,this.h),1)
-		this.maze.changeinrange((this.w-1,this.w),(0,this.h),1)
-		this.maze.changeinrange((0,this.w),(0,0),1)
-		this.maze.changeinrange((0,this.w),(this.h-1,this.h),1)
+		console.log(this.maze.show);
+		this.maze.changeInRange([0, 0], [0, this.h], 1)
+		this.maze.changeInRange([this.w - 1, this.w], [0, this.h], 1)
+		this.maze.changeInRange([0, this.w], [0, 0], 1)
+		this.maze.changeInRange([0, this.w], [this.h-1, this.h], 1)
+		console.log(this.maze.show);
 
 		for (var i = 0; i < this.h*this.w; i++){
-			horizontalWork;
-			verticalWork;
+			this.horizontalWork;
+			this.verticalWork;
 		}
-		corrector;
+		this.corrector;
 	}
 
 	randomRange(a, b){
@@ -27,7 +29,7 @@ class Maze{
 	}
 
 	horizontalWork(){
-		var num_col = randomRange(1, this.w-1);
+		var num_col = this.randomRange(1, this.w-1);
 		var num_row = 1;
 
 		var start_point = [-1, -1];
@@ -56,7 +58,7 @@ class Maze{
 			return false;
 		}
 
-		var end [-1, -1];
+		var end = [-1, -1];
 
 		while (true){
 			var vpos = this.maze.getLocal(num_row,num_col);
@@ -73,18 +75,98 @@ class Maze{
 				break;
 			}
 		}
-		num_mid = randomRange(start_point[0], start_point[1]);
+		num_mid = this.randomRange(start_point[0], start_point[1]);
 
-		this.maze.changeInRange([num_col, num_col], [start_point[0], end[0], 1);
+		this.maze.changeInRange([num_col, num_col], [start_point[0], end[0]], 1);
 		this.maze.change(num_mid, num_col, 0);
 
 		return true;
 	}
 
-	
+	verticalWork(){
+		var num_row = this.randomRange(1, this.h-1);
+		var num_col = 1;
+		var start = [-1,-1];
+		var work = false;
 
+		while (true){
+			var vpos = this.maze.getLocal(num_row, num_col);
+			var vposl = this.maze.getLocal(num_row - 1, num_col);
+			var vposr = this.maze.getLocal(num_row + 1, num_col);
+			var vnpos = this.maze.getLocal(num_row, num_col + 1);
+			var vnposl = this.maze.getLocal(num_row - 1, num_col + 1);
+			var vnposr = this.maze.getLocal(num_row + 1, num_col + 1);
+			if(vpos == 0 && vposl == 0 && vposr == 0 && vnpos == 0 && vnposl == 0 && vnposr == 0){
+				start = [num_row, num_col]; 
+				work = frue;
+				break;
+			}
+			else{
+				num_col = num_col + 1
+				if (num_col >= this.h - 1){
+					break;
+				}
+			}
+		}
+		if (work == false){
+			return false;
+		}
+		var end = [-1, -1];
+		while (true){
+			var vpos = this.maze.getLocal(num_row,num_col);
+			var vposl = this.maze.getLocal(num_row-1,num_col);
+			var vposr = this.maze.getLocal(num_row+1,num_col);
+			var vnpos = this.maze.getLocal(num_row,num_col+1);
+			var vnposl = this.maze.getLocal(num_row-1,num_col+1);
+			var vnposr = this.maze.getLocal(num_row+1,num_col+1);
 
+			if(vpos == 0 && vposl == 0 && vposr == 0 && vnpos == 0 && vnposl == 0 && vnposr == 0){
+				num_col = num_col + 1;
+			}
+			else{
+				end = [num_row, num_col + 1];
+				break;
+			}
+		}
+		var num_mid = this.randomRange(start[1], end[1]);
+		this.maze.changeInRange([start[1], end[1]], [num_row, num_row], 1);
+		this.maze.change(num_row, num_mid, 0);
+		return true;
+	}
 
+	corrector(){
+		for(var i = 1; i < self.w; i++){
+			for(var o = 1; o < self.h;  o++){
+				var M = this.maze;
+				if (M.getLocal(i, o) == 1){
+					if (M.getLocal(i, o - 1) == 0 && M.getLocal(i - 1, o - 1) == 1 && M.getLocal(i + 1, o - 1) == 1 && M.getLocal(i - 1, o) == 0 && M.getLocal(i + 1, o) == 0){
+						M.change(i, o, 0);
+					}
+					else if (M.getLocal(i, o + 1) == 0 && M.getLocal(i - 1, o + 1) == 1 && M.getLocal(i + 1, o + 1) == 1 && M.getLocal(i - 1, o) == 0 && M.getLocal(i + 1, o) == 0){
+						M.change(i, o, 0);
+					}
+					else if(M.getLocal(i - 1, o) == 0 && M.getLocal(i - 1, o - 1) == 1 && M.getLocal(i - 1, o + 1) == 1 && M.getLocal(i, o + 1) == 0 && M.getLocal(i, o - 1) == 0){
+						M.change(i, o, 0);
+					}
+					else if(M.getLocal(i + 1, o) == 0 && M.getLocal(i + 1, o - 1) == 1 && M.getLocal(i + 1, o + 1) == 1 && M.getLocal(i, o + 1) == 0 && M.getLocal(i, o - 1) == 0){
+						M.change(i, o, 0);
+					}
+				}
+
+			}
+
+		}
+
+	}
+
+	get getMaze(){
+		return this.maze;
+	}
+
+	get show(){
+		var m = this.maze.show;
+		return m;
+	}
 
 }
 	
