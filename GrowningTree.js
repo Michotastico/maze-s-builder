5738bddf7facb65589b913e98d3c32a3103d3637
Michel Llorens <mllorens@dcc.uchile.cl>
2016-02-09 01:00:42 -0300

    Implementation of Growning tree algorithm.

--- GrowningTree.js
+++ GrowningTree.js
@@ -49,13 +49,48 @@ function GrowningTree( method, height, weight){
     this.w = weight;
     this.method = method;
 
-    this.run(){
+    this.run = function(){
         var _x = Math.floor(Math.random() * this.h);
         var _y = Math.floor(Math.random() * this.w);
         var _node = this.maze[_x][_y];
+        _node.visited = true;
         this.active_set.push(_node);
         while(this.active_set.length != 0){
-            //TODO Select node, check neighbors. etc etc.
+            //Select the node and remove it from the active_set.
+            var result = this.method(this.active_set);
+            this.active_set = result[0];
+            _node = result[1];
+            //Set the directions of neighbors
+            var directions = [[0, -1], [0, 1], [-1, 0], [1, 0]];  //UP, DOWN, LEFT, RIGHT
+            //Set the undefined neighbor, remove flag and the index.
+            var neighbor = undefined;
+            var remove = false;
+            var _i = -1;
+            // Find a valid neighbor or check the remove flag.
+            while(neighbor == undefined){
+                if(directions.length == 0){
+                    remove = true;
+                    break;
+                }
+                _i = Math.floor(Math.random() * directions.length);
+                var _d = directions[_i]
+                directions.splice(_i, 1);
+                var column = this.maze[_node._x + _d[0]]
+                if(column == undefined){
+                    continue;
+                }
+                neighbor = column[_node._y + _d[1]];
+                if(neighbor != undefined && neighbor.wasVisited()){
+                    neighbor = undefined;
+                }
+            }
+            if(remove){
+                continue;
+            }
+            _node.sides[_i] = false;
+            neighbor.visited = true;
+            this.active_set.push(_node);
+            this.active_set.push(neighbor);
         }
     }
 
@@ -66,10 +101,10 @@ function GrowningTree( method, height, weight){
                 this.maze[i][j] = new MazeNode(i, j);
             }
         }
+        this.run();
+        console.log(this.maze);
     }
 
     this.init();
-
-
 
 }
